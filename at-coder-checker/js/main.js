35ee3b8f87e77e56d1e5d968899dcfacc305cb2a
snakazawa <nakazawa.tms0314@gmail.com>
2014-08-04 19:42:13 +0900

    変数名のリファクタリング

--- at-coder-checker/js/main.js
+++ at-coder-checker/js/main.js
@@ -19,9 +19,9 @@ $(function () {
 
     vm.users = user_model.users;
     vm.users_display = ko.observableArray();
-    vm.contest_list = ko.observableArray();
-    vm.current_contest = ko.observable();
-    vm.current_contest_id = ko.observable();
+    vm.contestList = ko.observableArray();
+    vm.currentContest = ko.observable();
+    vm.currentContestId = ko.observable();
     vm.submissions = ko.observableArray();
     vm.problems = ko.observable();
 
@@ -32,12 +32,12 @@ $(function () {
     });
 
     contest_model.contests.subscribe(function (contests) {
-        vm.contest_list(contests);
+        vm.contestList(contests);
     });
 
     // current_contest_idに追従してcurrent_contestも更新
-    vm.current_contest_id.subscribe(function (contest_id) {
-        vm.current_contest(contest_id ?
+    vm.currentContestId.subscribe(function (contest_id) {
+        vm.currentContest(contest_id ?
             _.where(contest_model.contests(), { contest_id: contest_id })[0] : null);
     });
 
@@ -45,20 +45,20 @@ $(function () {
      * 現在のコンテストを更新する
      *
      */
-    vm.update_current_contest = function () {
-        if (!vm.current_contest_id()) {
+    vm.updateCurrentContest = function () {
+        if (!vm.currentContestId()) {
             return $.Deferred().reject();
         }
 
         return $.when(
             // 他の人がユーザリストを更新した可能性もあるため、ユーザリストも一緒に更新する
             user_model.fetchUsers(),
-            submission_model.fetchSubmission(vm.current_contest_id())
+            submission_model.fetchSubmission(vm.currentContestId())
         ).done(function () {
             var fetched_submission = submission_model.lastFetchSubmission(),
                 users = _.pluck(user_model.users(), 'user_id'),
                 problems = fetched_submission.problems,
-                contest = vm.current_contest(),
+                contest = vm.currentContest(),
                 // 表示用にサブミッションリストを整形する。
                 submissions_ary = _.map(fetched_submission.problems, function (problem) {
                     var submissions = _.indexBy(problem.submissions, 'user_id');
@@ -83,14 +83,14 @@ $(function () {
     };
 
     // 選択しているコンテストが変更されたら、コンテストを更新
-    vm.current_contest_id.subscribe(function () {
-        vm.update_current_contest();
+    vm.currentContestId.subscribe(function () {
+        vm.updateCurrentContest();
     });
 
     // ユーザ追加に成功したら、現在のコンテストも更新
     (function (f) {
         add_user_view_model.add = function () {
-            return f.apply(this, arguments).then(vm.update_current_contest);
+            return f.apply(this, arguments).then(vm.updateCurrentContest);
         };
     }(add_user_view_model.add));
 
@@ -106,7 +106,7 @@ $(function () {
                         new_contest = _.where(contests, { url:  url });
 
                     if (new_contest.length) {
-                        vm.current_contest_id(new_contest[0].contest_id);
+                        vm.currentContestId(new_contest[0].contest_id);
                     }
                 });
         };
@@ -129,7 +129,7 @@ $(function () {
     // ローディングの設定
     loading_view_model.wrapDeferredAll(add_user_view_model, ['add']);
     loading_view_model.wrapDeferredAll(add_contest_view_model, ['add']);
-    loading_view_model.wrapDeferredAll(vm, ['update_current_contest']);
+    loading_view_model.wrapDeferredAll(vm, ['updateCurrentContest']);
 
     //
 
@@ -137,5 +137,5 @@ $(function () {
 
     ko.applyBindings(vm);
 
-    setTimeout(function () { vm.current_contest_id(13); }, 1000);
+    setTimeout(function () { vm.currentContestId(13); }, 1000);
 });
